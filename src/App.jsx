c328330a36461f9cce4e2c44d2ee5cebaeed0c06
Saikrishnacs <saikrishnaheerakari@gmail.com>
2025-05-02 15:18:02 +0530

    custom button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import React from 'react';
+import CustomButton from './Components/Buttons/index'
 
-
+   /*you need to apply styles to customButton pass props */ 
 function App() {
 
   return (
     <>
-    
+ 
+  <CustomButton
+  backgroundColor='black'
+  radius='5px'
+>
+
+  </CustomButton>
     </>
   )
 }
---
+++ src/Components/Buttons/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const CustomButton=(
+    {height="45px",
+    // width="175px",
+    radius="1px",
+    label="button",
+    backgroundColor="#FFF",
+    color="#8A8C8F",
+    border="none",
+    click=()=>{}}
+)=>{
+ const buttonStyle={
+    height,
+    // width,
+    backgroundColor,
+    borderRadius: radius,
+    color:color,
+    fontSize: '16px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+    padding:"9px 18px",
+    display:"flex",
+    alignItems:"center",
+    justifyContent: 'center',
+    border: border !== 'none' ? '2px solid #6B728080' : 'none',
+    
+  };
+  return(
+    <button style={buttonStyle} onClick={click}>
+        {label}
+    </button>
+  )
+ }
+export default CustomButton;
